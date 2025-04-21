4f3a3054b8635595184537d8099e405f0523b8c5
Enzo Patti <enzo.patti@aluno.senai.br>
2025-04-21 12:44:34 -0300

    style(pages): remove background from individual pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,19 @@
 export default function LandingPage() {
  return (
   <div className="overflow-x-hidden">
-   {/* Seção Início */}
-   <section
-    id="inicio"
-    className="flex justify-center items-center h-screen bg-blue-500"
-   >
+   <section id="inicio" className="flex justify-center items-center h-screen">
     <h1 className="text-4xl font-bold text-white">Início</h1>
    </section>
 
-   {/* Seção Sobre nós */}
-   <section
-    id="sobre"
-    className="flex justify-center items-center h-screen bg-green-500"
-   >
+   <section id="sobre" className="flex justify-center items-center h-screen">
     <h1 className="text-4xl font-bold text-white">Sobre nós</h1>
    </section>
 
-   {/* Seção Catálogo */}
-   <section
-    id="catalogo"
-    className="flex justify-center items-center h-screen bg-red-500"
-   >
+   <section id="catalogo" className="flex justify-center items-center h-screen">
     <h1 className="text-4xl font-bold text-white">Catálogo</h1>
    </section>
 
-   {/* Seção Fale Conosco */}
-   <section
-    id="contato"
-    className="flex justify-center items-center h-screen bg-purple-500"
-   >
+   <section id="contato" className="flex justify-center items-center h-screen">
     <h1 className="text-4xl font-bold text-white">Fale Conosco</h1>
    </section>
   </div>
